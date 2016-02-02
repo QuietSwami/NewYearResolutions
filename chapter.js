01e5c83a95bce06e7cee9e05462d3808b76de9a0
QuietSwami <fran.abm94@gmail.com>
2016-02-02 15:18:09 +0000

    Finished the chapter.js

--- chapter.js
+++ chapter.js
@@ -1,13 +1,19 @@
 $(document).ready(function($) {
-	if (!localStorage.getItem("chapter")){
+	if (!localStorage.getItem("chapter") && !localStorage.getItem("dailyChapter")){
 		localStorage.setItem("chapter", 0);
-	}
-	else{
-		$(".dailyChapter").click(function(){
-			var chapters = parseInt(localStorage.getItem("chapter"));
-			chapters += 1;
-			console.log(chapters);
-			localStorage.setItem("chapter", chapters);
-		});
+		localStorage.setItem("dailyChapter", "");
+	}else{
+		var date = moment().format("DD-MMM-YYYY");
+		if (localStorage.getItem("dailyChapter") != date){
+			$(".dailyChapter").click(function(){
+				var chapters = parseInt(localStorage.getItem("chapter"));
+				chapters += 1;
+				localStorage.setItem("chapter", chapters);
+				localStorage.setItem("dailyChapter", date);
+			});
+		}
+		else{
+			$(".dailyChapter").prop("checked", true);
+		}
 	}
 });
